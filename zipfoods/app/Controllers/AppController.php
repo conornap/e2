de98b9630e866c6595c53cbf3cd69876f48d6697
Conor Napier <conor.napier@gmail.om>
2019-11-05 21:35:32 -0500

    update to zipfood

--- zipfoods/app/Controllers/AppController.php
+++ zipfoods/app/Controllers/AppController.php
@@ -14,4 +14,10 @@ class AppController extends Controller
             'welcome' => $welcomes[array_rand($welcomes)]
         ]);
     }
+    public function contact(){
+        return  $this->app->view('contact');
+    }
+    public function about(){
+        return  $this->app->view('about');
+    }
 }
